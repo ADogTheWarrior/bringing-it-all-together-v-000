3791b76f815a4096f56248fcf66e9412cb070925
Adam Fudge <coolfudge@gmail.com>
2017-11-11 17:42:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,7 @@ class Dog
   attr_accessor :name, :breed, :id
 
   def initialize(attributes)
+    binding.pry
     @id = nil
     attributes.each {|key, value| self.send(("#{key}="), value)}
     self
