878c0746dec5591e17647bb481ab4edc68591078
Adam Fudge <coolfudge@gmail.com>
2017-11-11 17:47:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,10 +36,10 @@ class Dog
   def self.find_or_create_by(attributes)
   end
 
-  def self.new_from_db
+  def self.new_from_db(attributes)
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
   end
 
   def update
