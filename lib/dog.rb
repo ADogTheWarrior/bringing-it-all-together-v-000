2358bef6331ee133139fab08a2a1b0af5dbbed7d
Adam Fudge <coolfudge@gmail.com>
2017-11-11 18:00:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,7 +60,7 @@ class Dog
   def self.find_by_name(name)
     sql = "SELECT * FROM songs WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Dog.new(result[0], result[1], result[2])
+    Dog.new_by_row(result)
   end
 
   def update
